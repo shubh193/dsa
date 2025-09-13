853fce31a31668ff4ca85fb2de6951fff91ffb20
shubh193 <shubhcse193@gmail.com>
2025-09-13 10:14:41 +0530

    pattern1 completed

--- pattern/Pattern.java
+++ pattern/Pattern.java
@@ -1,7 +1,18 @@
 package pattern;
 
 public class Pattern {
+
+    static void pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.err.println("");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        int n = 5;
+        pattern1(n);
     }
 }
