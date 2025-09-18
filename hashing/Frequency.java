d5e9d15d058679628d8a3a656679013545b5fbf0
shubh193 <shubhcse193@gmail.com>
2025-09-18 12:42:01 +0530

    character count

--- hashing/Frequency.java
+++ hashing/Frequency.java
@@ -15,7 +15,7 @@ public class Frequency {
     }
 
     /*
-     * Better approach: (2 steps)
+     * Better approach: hashing (2 steps)
      * pre-store and fetching
      * if array size increases then will not handled
      */
@@ -29,9 +29,32 @@ public class Frequency {
         return hasharray[q];
     }
 
+    // brute borce approcah
+    static int characterCount1(String str, char c) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c)
+                count += 1;
+        }
+        return count;
+    }
+
+    // using hashing
+    static int characterCount2(String str, char c) {
+        int count = 0;
+        int hash[] = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            hash[str.charAt(i) - 'a'] += 1;
+        }
+        return hash[c - 'a'];
+    }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int num[] = { 1, 2, 2, 3, 1, 2, 5, 6, 6, 7, 8, 8, 8 };
         System.out.println(frequencyCount2(num, 8));
+        String str = "skhuchkuccmmmtyyyyy";
+        System.out.println(characterCount2(str, 'y'));
     }
 }
